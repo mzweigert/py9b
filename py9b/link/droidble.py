ed727ea440b1ab024db8e6389cb83e83d22e691b
Slinger360 <dilsha21@gmail.com>
2019-10-29 22:44:33 -0500

    added return to read and some feedback

--- py9b/link/droidble.py
+++ py9b/link/droidble.py
@@ -19,43 +19,35 @@ try:
 except ImportError:
     import Queue as queue
 
-SCAN_TIMEOUT = 3
+from threading import Event
 
-_write_chunk_size = 20
+try:
+    from kivymd.toast import toast
+except:
+    print('no toast for you')
+
 
 identity = bytearray(
     [
-        0x4E,
-        0x42,
-        0x21,
-        0x00,
-        0x00,
-        0x00,
-        0x00,
-        0xDE,  # Ninebot Bluetooth ID 4E422100000000DE
-        0x4E,
-        0x42,
-        0x21,
-        0x00,
-        0x00,
-        0x00,
-        0x00,
-        0xDF,  # Xiaomi Bluetooth ID 4E422100000000DF
+        0x4E, 0x42, 0x21, 0x00, 0x00, 0x00, 0x00, 0xDE,  # Ninebot Bluetooth ID 4E422100000000DE
+        0x4E, 0x42, 0x21, 0x00, 0x00, 0x00, 0x00, 0xDF,  # Xiaomi M365 Bluetooth ID 4E422100000000DF
+        0x4E, 0x42, 0x22, 0x01, 0x00, 0x00, 0x00, 0xDC,  # Xiaomi M365 Pro Bluetooth ID 4E422201000000DC
     ]
 )
 
 service_ids = {"retail": "6e400001-b5a3-f393-e0a9-e50e24dcca9e"}  # service UUID
 
 receive_ids = {
-    "retail": "6e400002-b5a3-f393-e0a9-e50e24dcca9e"  # receive characteristic UUID
+    "retail": "6e400002-b5a3-f393-e0a9-e50e24dcca9e"# receive characteristic UUID
 }
 
 transmit_ids = {
-    "retail": "6e400003-b5a3-f393-e0a9-e50e24dcca9e"  # transmit characteristic UUID
+    "retail": "6e400003-b5a3-f393-e0a9-e50e24dcca9e"# transmit characteristic UUID
 }
 
-scoot_found = False
 
+SCAN_TIMEOUT = 5
+_write_chunk_size = 20
 
 class Fifo:
     def __init__(self):
@@ -66,23 +58,25 @@ class Fifo:
             self.q.put(b)
 
     def read(self, size=1, timeout=None):  # but read string
-        res = ""
-        for i in xrange(size):
-            res += chr(self.q.get(True, timeout))
+        res = bytearray()
+        for i in range(size):
+            res.append(self.q.get(True, timeout))
         return res
 
 
-class ScootBT(BluetoothDispatcher):
+class BLE(BluetoothDispatcher):
     def __init__(self):
-        super(ScootBT, self).__init__()
+        super(BLE, self).__init__()
         self.rx_fifo = Fifo()
+        self.addr = ''
         self.ble_device = None
+        self.scoot_found = False
         self.state = StringProperty()
-        self.dump = True
         self.tx_characteristic = None
         self.rx_characteristic = None
         self.timeout = SCAN_TIMEOUT
-        self.set_queue_timeout(self.timeout)
+        self.dump = True
+        self.connected = Event()
 
     def __enter__(self):
         return self
@@ -90,19 +84,24 @@ class ScootBT(BluetoothDispatcher):
     def __exit__(self, exc_type, exc_value, traceback):
         self.close()
 
-    def discover(self):
+    def discover(self, timeout):
         self.start_scan()
         self.state = "scan"
-        print(self.state)
+        try:
+            toast(self.state)
+        except:
+            print(self.state)
+        self.connected.wait(timeout)
+        self.stop_scan()
+
 
     def on_device(self, device, rssi, advertisement):
-        global scoot_found
         if self.state != "scan":
             return
         Logger.debug("on_device event {}".format(list(advertisement)))
-        self.addr = device.getAddress()
+        address = device.getAddress()
         if self.addr and address.startswith(self.addr):
-            print(self.addr)
+            print(address)
             self.ble_device = device
             self.scoot_found = True
             self.stop_scan()
@@ -110,37 +109,33 @@ class ScootBT(BluetoothDispatcher):
             for ad in advertisement:
                 print(ad)
                 if ad.ad_type == Advertisement.ad_types.manufacturer_specific_data:
-                    if ad.data.startswith(self.identity):
-                        scoot_found = True
+                    if ad.data.startswith(identity):
+                        self.scoot_found = True
                     else:
                         break
                 elif ad.ad_type == Advertisement.ad_types.complete_local_name:
                     name = str(ad.data)
-        if scoot_found:
+        if self.scoot_found:
             self.state = "found"
-            print(self.state)
+            try:
+                toast(self.state)
+            except:
+                print(self.state)
             self.ble_device = device
             Logger.debug("Scooter detected: {}".format(name))
             self.stop_scan()
 
     def on_scan_completed(self):
-        if self.ble_device:
+        if self.ble_device and self.state != "connected":
             self.connect_gatt(self.ble_device)
-            self.state = "connected"
-            print(self.state)
         else:
-            self.start_scan()
+            self.close()
 
     def on_connection_state_change(self, status, state):
-        if status == GATT_SUCCESS and state:
+        if self.ble_device and self.state != "connected":
             self.discover_services()
-            self.state = "discover"
-            print(self.state)
         else:
-            self.close_gatt()
-            self.rx_characteristic = None
-            self.tx_characteristic = None
-            self.services = None
+            self.close()
 
     def on_services(self, status, services):
         self.services = services
@@ -150,31 +145,45 @@ class ScootBT(BluetoothDispatcher):
         for uuid in transmit_ids.values():
             self.tx_characteristic = self.services.search(uuid)
             print("TX: " + uuid)
-            self.enable_notifications(self.tx_characteristic)
-
-    def on_characteristic_changed(self, characteristic):
-        if characteristic == self.tx_characteristic:
-            data = characteristic.getValue()
-            self.rx_fifo.write(data)
-
-    def open(self, port):
-        self.addr = port
-        if self.ble_device == None:
-            self.discover()
-        if self.state != "connected":
-            self.connect_gatt(self.ble_device)
+            self.enable_notifications(self.tx_characteristic, enable=True)
+        if self.tx_characteristic and self.rx_characteristic:
+            self.connected.set()
+            self.state = "connected"
+            try:
+                toast(self.state)
+            except:
+                print(self.state)
         else:
             return
 
+    def on_characteristic_changed(self, tx_characteristic):
+        if self.tx_characteristic:
+            data = self.tx_characteristic.getValue()
+            self.rx_fifo.write(data)
+            return
+
+    def open(self, port):
+        self.addr = port
+        if not self.ble_device:
+            self.scan()
+        if self.ble_device and self.state != "connected":
+            self.connect_gatt(self.ble_device)
+
     def close(self):
-        if self.ble_device != None:
+        if self.ble_device and self.state == 'connected':
             self.close_gatt()
         self.services = None
-        print("close")
+        self.rx_characteristic = None
+        self.tx_characteristic = None
+        self.connected.clear()
+        self.state = "close"
+        try:
+            toast(self.state)
+        except:
+            print(self.state)
 
     def read(self, size):
-        print("read")
-        if self.ble_device:
+        if self.ble_device and self.state == 'connected':
             try:
                 data = self.rx_fifo.read(size, timeout=self.timeout)
             except queue.Empty:
@@ -182,13 +191,9 @@ class ScootBT(BluetoothDispatcher):
             if self.dump:
                 print("<", hexlify(data).upper())
             return data
-        else:
-            print("BLE not connected")
-            self.discover()
 
     def write(self, data):
-        print("write")
-        if self.ble_device:
+        if self.ble_device and self.state == 'connected':
             if self.dump:
                 print(">", hexlify(data).upper())
             size = len(data)
@@ -200,12 +205,9 @@ class ScootBT(BluetoothDispatcher):
                 )
                 ofs += chunk_sz
                 size -= chunk_sz
-        else:
-            print("BLE not connected")
-            self.discover()
 
     def scan(self):
-        self.discover()
+        self.discover(SCAN_TIMEOUT)
 
 
 class BLELink(BaseLink):
@@ -214,15 +216,15 @@ class BLELink(BaseLink):
         self._adapter = None
 
     def __enter__(self):
-        self._adapter = ScootBT()
-        self._adapter.discover()
+        self._adapter = BLE()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.close()
+        if self._adapter:
+            self.close()
 
-    def scan(self):
-        devices = self._adapter.scan()
+    def scan(self, timeout=SCAN_TIMEOUT):
+        self._adapter.scan()
 
     def open(self, port):
         self._adapter.open(port)
@@ -231,10 +233,10 @@ class BLELink(BaseLink):
         self._adapter.close()
 
     def read(self, size):
-        self._adapter.read(size)
+        return self._adapter.read(size)
 
     def write(self, data):
-        self._adapter.write(data)
+        self._adapter.write(bytearray(data))
 
 
 __all__ = ["BLELink"]
